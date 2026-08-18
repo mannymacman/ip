6be91f5c43827fac7cf8850f18c2710854e32c6b
k3ithzz <k3ith.tang@gmail.com>
2026-08-18 15:26:08 +0800

    Added Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String name;
+    private boolean isDone = false;
+
+    public Task(String name) {
+        this.name = name;
+    }
+    
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        if (isDone) {
+            return String.format("[X] %s", this.name);
+        } else {
+            return String.format("[ ] %s", this.name);
+        }
+    }
+}
