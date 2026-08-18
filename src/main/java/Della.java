107e43d84bfbf19a28f1dc6639413ff994f24cfc
k3ithzz <k3ith.tang@gmail.com>
2026-08-18 14:38:50 +0800

    Add support for echoing commands

--- src/main/java/Della.java
+++ src/main/java/Della.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Della {
     public static void main(String[] args) {
         String banner =
@@ -10,8 +12,27 @@ public class Della {
         String farewell = "Byee! Rest well!";
 
         System.out.println(banner);
-        System.out.println("----------------------------------------");
+        System.out.println("========================================");
         System.out.println(greeting);
+        System.out.println("========================================");
+
+        String command = "";
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            command = s.nextLine();
+
+            // exit when bye command given
+            if (command.equals("bye")) {
+                break;
+            }
+
+            // echo command
+            System.out.println("    ----------------------------------------");
+            System.out.println("    " + command);
+            System.out.println("    ----------------------------------------");
+        }
+        s.close();
+        System.out.println("========================================");
         System.out.println(farewell);
     }
 }
