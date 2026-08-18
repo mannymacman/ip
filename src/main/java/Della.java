3ee8bea41453aef0fb70af23e73727ec96507ac7
k3ithzz <k3ith.tang@gmail.com>
2026-08-18 13:12:22 +0800

    Add greeting and exit

--- src/main/java/Della.java
+++ src/main/java/Della.java
@@ -6,6 +6,12 @@ public class Della {
                 "   D   D  eee   l      l      aaaaaa\n" +
                 "   D   D  e     l      l      a    a\n" +
                 "   DDDD   eeee  llll   llll   a    a";
+        String greeting = "Hi! I'm Della :))\nHow can I help you?";
+        String farewell = "Byee! Rest well!";
+
         System.out.println(banner);
+        System.out.println("----------------------------------------");
+        System.out.println(greeting);
+        System.out.println(farewell);
     }
 }
