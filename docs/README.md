0f01abd069efebcc88ce465671521de07988f238
k3ithzz <k3ith.tang@gmail.com>
2026-08-18 13:02:37 +0800

    Removed every instance of Duke and replaced it with Della

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Della User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Della.java
@@ -0,0 +1,11 @@
+public class Della {
+    public static void main(String[] args) {
+        String banner =
+                "   DDDD   eeee  l      l       aaaa\n" +
+                "   D   D  e     l      l      a    a\n" +
+                "   D   D  eee   l      l      aaaaaa\n" +
+                "   D   D  e     l      l      a    a\n" +
+                "   DDDD   eeee  llll   llll   a    a";
+        System.out.println(banner);
+    }
+}
