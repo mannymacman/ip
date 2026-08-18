bd781e54fed2848f719e94cbe0dd0367e21412cb
k3ithzz <k3ith.tang@gmail.com>
2026-08-18 18:06:11 +0800

    Add support for marking and unmarking tasks

--- src/main/java/Della.java
+++ src/main/java/Della.java
@@ -33,12 +33,62 @@ public class Della {
                     System.out.printf("    %d. %s\n", i + 1, tasks.get(i));
                 }
                 System.out.println("    ----------------------------------------");
-            } else { // if just adding tasks
-                Task newTask = new Task(command);
-                tasks.add(newTask);
-                System.out.println("    ----------------------------------------");
-                System.out.printf("    added: %s\n", newTask);
-                System.out.println("    ----------------------------------------");
+            } else {
+                String[] commandParts = command.trim().split(" ");
+
+                if (commandParts.length == 2 && commandParts[0].equals("mark")) {
+                    // mark task
+                    try {
+                        int taskNum = Integer.parseInt(commandParts[1]);
+
+                        // check if task is within the number of tasks provided
+                        if (taskNum > 0 && taskNum <= tasks.size()) {
+                            Task task = tasks.get(taskNum - 1);
+                            task.mark();
+                            System.out.println("    ----------------------------------------");
+                            System.out.println("    Nice! I have marked this task as done:");
+                            System.out.println("      marked: " + task);
+                            System.out.println("    ----------------------------------------");
+                        } else {
+                            System.out.println("    ----------------------------------------");
+                            System.out.println("    Invalid task provided. Please try again!");
+                            System.out.println("    ----------------------------------------");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("    ----------------------------------------");
+                        System.out.println("    Invalid task provided. Please try again!");
+                        System.out.println("    ----------------------------------------");
+                    }
+                } else if (commandParts.length == 2 && commandParts[0].equals("unmark")) {
+                    // unmark task
+                    try {
+                        int taskNum = Integer.parseInt(commandParts[1]);
+
+                        // check if task is within the number of tasks provided
+                        if (taskNum > 0 && taskNum <= tasks.size()) {
+                            Task task = tasks.get(taskNum - 1);
+                            task.unmark();
+                            System.out.println("    ----------------------------------------");
+                            System.out.println("    OK, I've marked this task as not done yet:");
+                            System.out.println("      unmarked: " + task);
+                            System.out.println("    ----------------------------------------");
+                        } else {
+                            System.out.println("    ----------------------------------------");
+                            System.out.println("    Invalid task provided. Please try again!");
+                            System.out.println("    ----------------------------------------");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("    ----------------------------------------");
+                        System.out.println("    Invalid task provided. Please try again!");
+                        System.out.println("    ----------------------------------------");
+                    }
+                } else { // if just adding tasks
+                    Task newTask = new Task(command);
+                    tasks.add(newTask);
+                    System.out.println("    ----------------------------------------");
+                    System.out.printf("    added: %s\n", newTask);
+                    System.out.println("    ----------------------------------------");
+                }
             }
         }
         s.close();
