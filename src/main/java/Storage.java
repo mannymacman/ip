448278ee7cd2695ab44e735379d6248aa4d085c2
k3ithzz <k3ith.tang@gmail.com>
2026-08-25 17:52:27 +0800

    Update storeTask to throw IOexception

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,13 +2,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Storage {
-    public static void storeTask(Task task) {
-        try {
-            FileWriter fw = new FileWriter("./data/della.txt");
-            fw.write(task.toString());
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("Error when storing task!");
-        }
+    public static void storeTask(Task task) throws IOException {
+        FileWriter fw = new FileWriter("./data/della.txt");
+        fw.write(task.toString());
+        fw.close();
     }
 }
