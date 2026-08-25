a5b2bd975e6f4423d8aa517b15c6aba10091fd2d
k3ithzz <k3ith.tang@gmail.com>
2026-08-25 17:38:38 +0800

    Add storeTask method in Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Storage {
+    public static void storeTask(Task task) {
+        try {
+            FileWriter fw = new FileWriter("./data/della.txt");
+            fw.write(task.toString());
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error when storing task!");
+        }
+    }
 }
