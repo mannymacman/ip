206c07942aab4f26643783ce7613f49cef6e0b69
k3ithzz <k3ith.tang@gmail.com>
2026-08-26 22:16:39 +0800

    Add JUnit tests for DateParser and Storage

---
+++ src/test/java/della/util/DateParserTest.java
@@ -0,0 +1,32 @@
+package della.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DateParserTest {
+    @Test
+    public void parseDateTime_validDateAndPattern_dateTimeReturned() {
+        LocalDateTime expected = LocalDateTime.of(2026, 8, 26, 14, 30);
+
+        LocalDateTime actual = DateParser.parseDateTime("26/08/2026 14:30", "dd/MM/yyyy HH:mm");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parseDateTime_invalidTime_exceptionThrown() {
+        assertThrows(DateTimeParseException.class,
+                () -> DateParser.parseDateTime("26/08/2026 14:60", "dd/MM/yyyy HH:mm"));
+    }
+
+    @Test
+    public void parseDateTime_inputDoesNotMatchPattern_exceptionThrown() {
+        assertThrows(DateTimeParseException.class,
+                () -> DateParser.parseDateTime("2026-08-26 14:30", "dd/MM/yyyy HH:mm"));
+    }
+}
